62e9713ba2f01072e71f4f81f85ee7d3f3d63caa
ALEKSANDR GUBICH <a.gubich@bk.ru>
2025-09-16 21:47:35 +0300

    .

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { Card } from "./components/card/Card";
 import { Drawer } from "./components/drawer/Drawer";
 import { Header } from "./components/header/Header";
@@ -6,20 +7,29 @@ import { Header } from "./components/header/Header";
 function App() {
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
   const [cartOpened, setCartOpened] = useState(false);
 
   useEffect(() => {
-    fetch("https://68c45fba81ff90c8e61c0f7a.mockapi.io/items")
+    axios
+      .get("https://68c45fba81ff90c8e61c0f7a.mockapi.io/items")
       .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setItems(json);
+        setItems(res.data);
+      });
+    axios
+      .get("https://68c45fba81ff90c8e61c0f7a.mockapi.io/cart")
+      .then((res) => {
+        setCartItems(res.data);
       });
   }, []);
 
   const onAddToCart = (obj) => {
+    axios.post("https://68c45fba81ff90c8e61c0f7a.mockapi.io/cart", obj);
     setCartItems((prev) => [...prev, obj]);
+  };
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
   };
 
   return (
@@ -39,26 +49,39 @@ function App() {
       />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue
+              ? `Поиск по запросу: "${searchValue}"`
+              : "Все кроссовки"}
+          </h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск..." />
+            <input
+              placeholder="Поиск..."
+              onChange={onChangeSearchInput}
+              value={searchValue}
+            />
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Card
-              title={item.title}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onPlus={(obj) => {
-                onAddToCart(obj);
-              }}
-              onFavorite={() => {
-                console.log("Добавили в закладки");
-              }}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item, i) => (
+              <Card
+                key={i}
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onPlus={(obj) => {
+                  onAddToCart(obj);
+                }}
+                onFavorite={() => {
+                  console.log("Добавили в закладки");
+                }}
+              />
+            ))}
         </div>
       </div>
     </div>
